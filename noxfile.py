400a10cb824072279bc327f551308c5f30cf5607
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2020-05-29 18:31:24 -0700

    Add mypy

--- noxfile.py
+++ noxfile.py
@@ -64,3 +64,10 @@ def safety(session):
         )
         install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
+
+
+@nox.session(python=["3.8", "3.7"])
+def mypy(session):
+    args = session.posargs or locations
+    install_with_constraints(session, "mypy")
+    session.run("mypy", *args)
